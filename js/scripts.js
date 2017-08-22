3215176310a3b41aa14bef827cd6dd347eb9d43c
Keegan Ruebling and Riki Montgomery <keegan+riki@>
2017-08-22 16:16:09 -0700

    test if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,28 @@ function Ticket(movie, time, age, quantity) {
   this.movieQuantity = quantity;
 }
 
-Ticket.prototype.totalPrice = function() {
-  return '$' + ((this.movieName + this.movieTime) - this.movieAge) * this.movieQuantity;
-}
+// Ticket.prototype.totalPrice = function() {
+//   return '$' + ((this.movieName + this.movieTime) - this.movieAge) * this.movieQuantity;
+// }
 
 // front-end logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    $("ul#receipt").empty(newTicket);
 
     var inputtedMovie = parseFloat($('#movieName').val());
     var inputtedTime = parseFloat($('#movieTime').val());
     var inputtedAge = parseFloat($('#movieAdmission').val());
     var inputtedQuantity = parseFloat($('#movieQuantity').val());
 
+    var finalMovie = $("#movieName option:selected").text();
+    var finalTime = $("#movieTime option:selected").text();
+    var finalAge = $("#movieAdmission option:selected").text();
+    var finalQuantity = $("#movieQuantity option:selected").text();
+
     var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge, inputtedQuantity);
 
-    alert(newTicket.totalPrice());
-
+    $('ul#receipt').append(finalMovie + " " + finalTime + " " + finalAge + " " + finalQuantity + " " + newTicket.totalPrice());
   });
 });
